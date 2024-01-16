287e630d88f8fc289adf53eb4704da2972421305
FrancisMProjects <mingomba@outlook.com>
2024-01-15 21:02:49 -0600

    First commit

---
+++ TestPro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deploying on AWS");
+            Console.ReadLine();
+        }
+    }
+}
